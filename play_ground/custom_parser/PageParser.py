f47a1143887c4a7ccc39225b3001e5ce92a103cf
VladimirKrutko <krutkovova24@gmail.com>
2024-05-12 14:15:07 +0200

    Update .gitignore and add new files and directories

--- play_ground/custom_parser/PageParser.py
+++ play_ground/custom_parser/PageParser.py
@@ -15,6 +15,15 @@ class PageParser:
     
     def parse_json(self):
         page_json = json.loads(self.doc.find('script', type='application/ld+json').string)
-        return { key: page_json[key] if key in page_json.keys() else None for key in PageParser.PARSED_FILEDS}
-        
+        li_tag = self.doc.find_all('li', attrs={"data-test": lambda x: x and 'item-technologies' in x})
+        technologies = [ p.find_all('p')[0] if len(p.find_all('p'))>0 else None for p in li_tag]
+        technologies = [ p.text for p in technologies if p is not None]
+        # technologies = [ p.text for p in self.doc.select("//li[ contains(@data-test, 'item-technologies')]/p")]
+        print(f"tech {technologies}")
+        url = self.doc.find('meta', {'property': 'og:url'})['content']
+        print(f"url {len(url)}")
+        data =  { key: page_json[key] if key in page_json.keys() else None for key in PageParser.PARSED_FILEDS}
+        data['technologies'] = ",".join(technologies) if len(technologies) > 0 else None
+        data['url'] = url
+        return data
         
--- play_ground/loader.py
+++ play_ground/loader.py
@@ -4,9 +4,9 @@ import time
 class Loader:
     TIME_POWER = 1000000
     
-    def crawler(self, url, data_type):
+    def crawler(self, url, data_type, save_path = './temp_data_ml'):
         crawl_data = requests.get(url).text
         file_name = f"{data_type}_{int(time.time() * Loader.TIME_POWER )}.html"
-        with open(f'./temp_data/{file_name}', 'w') as file:
+        with open(f'./{save_path}/{file_name}', 'w') as file:
             file.write(crawl_data)
         return file_name
