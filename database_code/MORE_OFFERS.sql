38ac8deed536ac232f506992f2181f9b3fdc4ca2
Uladzimir <krutkovova24@gmail.com>
2024-11-11 14:24:42 +0000

    Update db schema

--- database_code/MORE_OFFERS.sql
+++ database_code/MORE_OFFERS.sql
@@ -1,139 +1,139 @@
 CREATE TABLE "site" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "domain" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                        id serial PRIMARY KEY,
+                        "name" varchar,
+                        "domain" varchar,
+                        "create_date" timestamp,
+                        "update_date" timestamp
 );
 
 CREATE TABLE "company" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "url" varchar,
-  "description" text,
-  "create_date" datetime,
-  "update_date" datetime
+                           id serial PRIMARY KEY,
+                           "name" varchar,
+                           "url" varchar,
+                           "description" text,
+                           "create_date" timestamp,
+                           "update_date" timestamp
 );
 
 CREATE TABLE "location" (
-  "id" integer PRIMARY KEY,
-  "site_id" integer,
-  "url" varchar,
-  "active" bool,
-  "create_date" datetime,
-  "update_date" datetime
+                            id serial PRIMARY KEY,
+                            "site_id" integer,
+                            "url" varchar,
+                            "active" bool,
+                            "create_date" timestamp,
+                            "update_date" timestamp
 );
 
 CREATE TABLE "offer" (
-  "id" integer,
-  "name" varchar,
-  "id_company" integer,
-  "id_position_level" integer,
-  "create_date" datetime,
-  "update_date" datetime,
-  "primary" key(id,name,id_company)
+                         "id" serial PRIMARY KEY,
+                         "name" varchar,
+                         "id_company" integer,
+                         "id_position_level" integer,
+                         "create_date" timestamp,
+                         "update_date" timestamp,
+                         UNIQUE (name, id_company)
 );
 
 CREATE TABLE "offer_location" (
-  "id" integer PRIMARY KEY,
-  "offer_id" integer,
-  "location_id" integer,
-  "id_position_level" integer,
-  "create_date" datetime,
-  "update_date" datetime
+                                  id serial PRIMARY KEY,
+                                  "offer_id" integer,
+                                  "location_id" integer,
+                                  "id_position_level" integer,
+                                  "create_date" timestamp,
+                                  "update_date" timestamp
 );
 
 CREATE TABLE "technology" (
-  "id" integer PRIMARY KEY,
-  "name" varchar UNIQUE,
-  "create_date" datetime,
-  "update_date" datetime
+                              id serial PRIMARY KEY,
+                              "name" varchar UNIQUE,
+                              "create_date" timestamp,
+                              "update_date" timestamp
 );
 
 CREATE TABLE "position_level" (
-  "id" integer PRIMARY KEY,
-  "level" varchar UNIQUE,
-  "create_date" datetime,
-  "update_date" datetime
+                                  id serial PRIMARY KEY,
+                                  "level" varchar UNIQUE,
+                                  "create_date" timestamp,
+                                  "update_date" timestamp
 );
 
 CREATE TABLE "offer_technology" (
-  "id" integer PRIMARY KEY,
-  "id_offer" integer,
-  "id_technology" integer,
-  "create_date" datetime,
-  "update_date" datetime
+                                    id serial PRIMARY KEY,
+                                    "id_offer" integer,
+                                    "id_technology" integer,
+                                    "create_date" timestamp,
+                                    "update_date" timestamp
 );
 
 CREATE TABLE "offer_data" (
-  "id" integer PRIMARY KEY,
-  "id_offer" integer,
-  "data" jsonb,
-  "id_original_language" integer,
-  "translated_data" jsonb,
-  "create_date" datetime,
-  "update_date" datetime
+                              id serial PRIMARY KEY,
+                              "id_offer" integer,
+                              "data" jsonb,
+                              "id_original_language" integer,
+                              "translated_data" jsonb,
+                              "create_date" timestamp,
+                              "update_date" timestamp
 );
 
 CREATE TABLE "country" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                           id serial PRIMARY KEY,
+                           "name" varchar,
+                           "create_date" timestamp,
+                           "update_date" timestamp
 );
 
 CREATE TABLE "city" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                        id serial PRIMARY KEY,
+                        "name" varchar,
+                        "create_date" timestamp,
+                        "update_date" timestamp
 );
 
 CREATE TABLE "offer_geography" (
-  "id" integer PRIMARY KEY,
-  "id_city" integer,
-  "id_country" integer,
-  "id_offer" integer,
-  "create_date" datetime,
-  "update_date" datetime
+                                   id serial PRIMARY KEY,
+                                   "id_city" integer,
+                                   "id_country" integer,
+                                   "id_offer" integer,
+                                   "create_date" timestamp,
+                                   "update_date" timestamp
 );
 
 CREATE TABLE "language" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                            id serial PRIMARY KEY,
+                            "name" varchar,
+                            "create_date" timestamp,
+                            "update_date" timestamp
 );
 
 CREATE TABLE "salary" (
-  "id" integer PRIMARY KEY,
-  "value" varchar,
-  "contract_type" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                          id serial PRIMARY KEY,
+                          "value" varchar,
+                          "contract_type" varchar,
+                          "create_date" timestamp,
+                          "update_date" timestamp
 );
 
 CREATE TABLE "offer_salary" (
-  "id" integer,
-  "id_salary" integer,
-  "id_offer" integer,
-  "create_date" datetime,
-  "update_date" datetime
+                                id serial PRIMARY KEY,
+                                "id_salary" integer,
+                                "id_offer" integer,
+                                "create_date" timestamp,
+                                "update_date" timestamp
 );
 
 CREATE TABLE "bussines_type" (
-  "id" integer,
-  "type" varchar,
-  "create_date" datetime,
-  "update_date" datetime
+                                 id serial PRIMARY KEY,
+                                 "type" varchar,
+                                 "create_date" timestamp,
+                                 "update_date" timestamp
 );
 
 CREATE TABLE "company_bussines_type" (
-  "id" integer,
-  "id_company" integer,
-  "id_bussines_type" integer,
-  "create_date" datetime,
-  "update_date" datetime
+                                         id serial PRIMARY KEY,
+                                         "id_company" integer,
+                                         "id_bussines_type" integer,
+                                         "create_date" timestamp,
+                                         "update_date" timestamp
 );
 
 ALTER TABLE "company_bussines_type" ADD FOREIGN KEY ("id_company") REFERENCES "company" ("id");
@@ -146,7 +146,7 @@ ALTER TABLE "offer_salary" ADD FOREIGN KEY ("id_offer") REFERENCES "offer" ("id"
 
 ALTER TABLE "location" ADD FOREIGN KEY ("site_id") REFERENCES "site" ("id");
 
-ALTER TABLE "company" ADD FOREIGN KEY ("id") REFERENCES "offer" ("id_company");
+ALTER TABLE "offer" ADD FOREIGN KEY ("id_company") REFERENCES "company" ("id");
 
 ALTER TABLE "offer_location" ADD FOREIGN KEY ("location_id") REFERENCES "location" ("id");
 
@@ -156,7 +156,7 @@ ALTER TABLE "offer" ADD FOREIGN KEY ("id_position_level") REFERENCES "position_l
 
 ALTER TABLE "offer_technology" ADD FOREIGN KEY ("id_offer") REFERENCES "offer" ("id");
 
-ALTER TABLE "technology" ADD FOREIGN KEY ("id") REFERENCES "offer_technology" ("id_technology");
+ALTER TABLE "offer_technology" ADD FOREIGN KEY ("id_technology") REFERENCES "technology" ("id");
 
 ALTER TABLE "offer_data" ADD FOREIGN KEY ("id_offer") REFERENCES "offer" ("id");
 
