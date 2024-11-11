98fcd2f0932cdfcd925d6447c7758139df167035
Uladzimir <krutkovova24@gmail.com>
2024-11-11 15:20:43 +0000

    cantinue to develop base project structrure

--- scripting/crawler/base_crawler.py
+++ scripting/crawler/base_crawler.py
@@ -2,6 +2,13 @@ from abc import ABC, abstractmethod
 
 class BaseCrawler(ABC):
     @abstractmethod
+    def to_recrwale(self, responce_result):
+        """
+        Conditition for check responce reslut 
+        """
+        pass
+    
+    @abstractmethod
     def crawl(self, task):
         """
         Base method that should be realize in all creawlers
