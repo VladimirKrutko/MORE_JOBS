61a4e6d6d159fb20b110d012130b3f1163c08f18
VladimirKrutko <krutkovova24@gmail.com>
2024-11-18 22:38:04 +0100

    Implement PlaywrightLogin class for enhanced login functionality and refactor Chrome_base.py to pracuj_pl.py with improved proxy handling

--- scripting/login/pracuj_pl.py
+++ scripting/login/pracuj_pl.py
@@ -4,47 +4,37 @@ from playwright.sync_api import sync_playwright
 from base_login import BaseLogin
 
 class PlaywrightLogin(BaseLogin):
-    def login(self, proxy):
+    def login(self, proxy=None):
         with sync_playwright() as p:
-            browser = p.chromium.launch(proxy={"server": proxy}, headless=False)
+            browser = p.chromium.launch(proxy={"server": proxy}, headless=False) if proxy else p.chromium.launch(headless=False)
             context = browser.new_context()
             page = context.new_page()
-
-            # Переход на страницу логина (например, login_url)
             login_url = "https://it.pracuj.pl/praca"
             page.goto(login_url, timeout=60000)
-
-            # Нажатие на кнопку принятия cookies
             page.locator("//button[@data-test=\"button-submitCookie\"]").click()
             page.wait_for_load_state("networkidle")
-            
-            # Извлекаем cookies и user agent
             cookies = context.cookies()
             user_agent = page.evaluate("() => navigator.userAgent")
-
             browser.close()
-
             return {
-                "proxy": self.proxy,
+                "proxy": proxy,
                 "cookies": cookies,
                 "user_agent": user_agent
             }
 
-# Использование
+# with open('proxy_list.json', 'r') as f:
+#     proxy_list = json.load(f)
 
-with open('proxy_list.json', 'r') as f:
-    proxy_list = json.load(f)
-
-valid_proxy = [proxy for proxy in proxy_list if not proxy['banned']]
+# valid_proxy = [proxy for proxy in proxy_list if not proxy['banned']]
 
 login_instance = PlaywrightLogin()
-for proxy in valid_proxy:
+# for proxy in valid_proxy:
     
-    try:
-        credentials = login_instance.login(proxy['url'])
-        break
-    except Exception as e:
-        print(e)
-        continue
-# credentials = login_instance.login( random.choice(valid_proxy).get('url'))
+#     try:
+#         credentials = login_instance.login(proxy['url'])
+#         break
+#     except Exception as e:
+#         print(e)
+#         continue
+credentials = login_instance.login()
 print(credentials)
