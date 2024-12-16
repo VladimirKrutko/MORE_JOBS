d510d81cb1e05bcdd17e1bc171ba5876fbac2455
VladimirKrutko <krutkovova24@gmail.com>
2024-12-16 21:54:51 +0100

    Refactor models in models.py: reorganize imports, remove PositionLevel model, and update relationships for Offer, OfferLocation, OfferTechnology, OfferData, OfferGeography, OfferResponsibilities, and OfferRequirements to enhance clarity and maintainability of the database schema.

--- scripting/loader/models/models.py
+++ scripting/loader/models/models.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, ForeignKey, Text, JSON
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from datetime import datetime
-
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column, Integer, String, Boolean, Text, ForeignKey, DateTime, JSON
+)
 Base = declarative_base()
+
 
 class Site(Base):
     __tablename__ = 'site'
@@ -15,6 +17,7 @@ class Site(Base):
     update_date = Column(DateTime, default=datetime.utcnow)
 
     locations = relationship("Location", back_populates="site")
+
 
 class Company(Base):
     __tablename__ = 'company'
@@ -29,6 +32,7 @@ class Company(Base):
     offers = relationship("Offer", back_populates="company")
     business_types = relationship("CompanyBusinessType", back_populates="company")
 
+
 class Location(Base):
     __tablename__ = 'location'
 
@@ -42,16 +46,6 @@ class Location(Base):
     site = relationship("Site", back_populates="locations")
     offer_locations = relationship("OfferLocation", back_populates="location")
 
-class PositionLevel(Base):
-    __tablename__ = 'position_level'
-
-    id = Column(Integer, primary_key=True)
-    level = Column(String, unique=True)
-    create_date = Column(DateTime, default=datetime.utcnow)
-    update_date = Column(DateTime, default=datetime.utcnow)
-
-    offers = relationship("Offer", back_populates="position_level")
-    offer_locations = relationship("OfferLocation", back_populates="position_level")
 
 class Offer(Base):
     __tablename__ = 'offer'
@@ -59,17 +53,18 @@ class Offer(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     id_company = Column(Integer, ForeignKey('company.id'))
-    id_position_level = Column(Integer, ForeignKey('position_level.id'))
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
     company = relationship("Company", back_populates="offers")
-    position_level = relationship("PositionLevel", back_populates="offers")
-    offer_locations = relationship("OfferLocation", back_populates="offer")
-    offer_technologies = relationship("OfferTechnology", back_populates="offer")
-    offer_data = relationship("OfferData", back_populates="offer")
-    offer_geographies = relationship("OfferGeography", back_populates="offer")
-    offer_salaries = relationship("OfferSalary", back_populates="offer")
+    locations = relationship("OfferLocation", back_populates="offer")
+    technologies = relationship("OfferTechnology", back_populates="offer")
+    data = relationship("OfferData", back_populates="offer")
+    salaries = relationship("OfferSalary", back_populates="offer")
+    geographies = relationship("OfferGeography", back_populates="offer")
+    responsibilities = relationship("OfferResponsibilities", back_populates="offer")
+    requirements = relationship("OfferRequirements", back_populates="offer")
+
 
 class OfferLocation(Base):
     __tablename__ = 'offer_location'
@@ -77,13 +72,13 @@ class OfferLocation(Base):
     id = Column(Integer, primary_key=True)
     offer_id = Column(Integer, ForeignKey('offer.id'))
     location_id = Column(Integer, ForeignKey('location.id'))
-    id_position_level = Column(Integer, ForeignKey('position_level.id'))
+    id_position_level = Column(Integer)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer = relationship("Offer", back_populates="offer_locations")
+    offer = relationship("Offer", back_populates="locations")
     location = relationship("Location", back_populates="offer_locations")
-    position_level = relationship("PositionLevel", back_populates="offer_locations")
+
 
 class Technology(Base):
     __tablename__ = 'technology'
@@ -93,7 +88,8 @@ class Technology(Base):
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer_technologies = relationship("OfferTechnology", back_populates="technology")
+    offers = relationship("OfferTechnology", back_populates="technology")
+
 
 class OfferTechnology(Base):
     __tablename__ = 'offer_technology'
@@ -101,11 +97,13 @@ class OfferTechnology(Base):
     id = Column(Integer, primary_key=True)
     id_offer = Column(Integer, ForeignKey('offer.id'))
     id_technology = Column(Integer, ForeignKey('technology.id'))
+    obligatory = Column(Boolean)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer = relationship("Offer", back_populates="offer_technologies")
-    technology = relationship("Technology", back_populates="offer_technologies")
+    offer = relationship("Offer", back_populates="technologies")
+    technology = relationship("Technology", back_populates="offers")
+
 
 class OfferData(Base):
     __tablename__ = 'offer_data'
@@ -113,15 +111,14 @@ class OfferData(Base):
     id = Column(Integer, primary_key=True)
     id_offer = Column(Integer, ForeignKey('offer.id'))
     data = Column(JSON)
-    requirements = Column(Text)
-    responsibilities = Column(Text)
     id_original_language = Column(Integer, ForeignKey('language.id'))
     translated_data = Column(JSON)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer = relationship("Offer", back_populates="offer_data")
+    offer = relationship("Offer", back_populates="data")
     language = relationship("Language", back_populates="offer_data")
+
 
 class Language(Base):
     __tablename__ = 'language'
@@ -133,6 +130,7 @@ class Language(Base):
 
     offer_data = relationship("OfferData", back_populates="language")
 
+
 class Country(Base):
     __tablename__ = 'country'
 
@@ -141,7 +139,8 @@ class Country(Base):
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer_geographies = relationship("OfferGeography", back_populates="country")
+    geographies = relationship("OfferGeography", back_populates="country")
+
 
 class City(Base):
     __tablename__ = 'city'
@@ -151,7 +150,8 @@ class City(Base):
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer_geographies = relationship("OfferGeography", back_populates="city")
+    geographies = relationship("OfferGeography", back_populates="city")
+
 
 class OfferGeography(Base):
     __tablename__ = 'offer_geography'
@@ -163,63 +163,56 @@ class OfferGeography(Base):
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    city = relationship("City", back_populates="offer_geographies")
-    country = relationship("Country", back_populates="offer_geographies")
-    offer = relationship("Offer", back_populates="offer_geographies")
+    city = relationship("City", back_populates="geographies")
+    country = relationship("Country", back_populates="geographies")
+    offer = relationship("Offer", back_populates="geographies")
 
-class Salary(Base):
-    __tablename__ = 'salary'
+
+class Responsibilities(Base):
+    __tablename__ = 'responsibilities'
 
     id = Column(Integer, primary_key=True)
-    value = Column(String)
-    contract_type = Column(String)
+    data = Column(Text)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    offer_salaries = relationship("OfferSalary", back_populates="salary")
+    offers = relationship("OfferResponsibilities", back_populates="responsibilities")
 
-class OfferSalary(Base):
-    __tablename__ = 'offer_salary'
+
+class OfferResponsibilities(Base):
+    __tablename__ = 'offer_responsibilities'
 
     id = Column(Integer, primary_key=True)
-    id_salary = Column(Integer, ForeignKey('salary.id'))
     id_offer = Column(Integer, ForeignKey('offer.id'))
+    id_responsibilities = Column(Integer, ForeignKey('responsibilities.id'))
+    obligatory = Column(Boolean)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    salary = relationship("Salary", back_populates="offer_salaries")
-    offer = relationship("Offer", back_populates="offer_salaries")
+    offer = relationship("Offer", back_populates="responsibilities")
+    responsibilities = relationship("Responsibilities", back_populates="offers")
 
-class BusinessType(Base):
-    __tablename__ = 'business_type'
+
+class Requirements(Base):
+    __tablename__ = 'requirements'
 
     id = Column(Integer, primary_key=True)
-    type = Column(String)
+    data = Column(Text)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    company_business_types = relationship("CompanyBusinessType", back_populates="business_type")
+    offers = relationship("OfferRequirements", back_populates="requirements")
 
-class CompanyBusinessType(Base):
-    __tablename__ = 'company_business_type'
 
-    id = Column(Integer, primary_key=True)
-    id_company = Column(Integer, ForeignKey('company.id'))
-    business_type_id = Column(Integer, ForeignKey('business_type.id'))
-    create_date = Column(DateTime, default=datetime.utcnow)
-    update_date = Column(DateTime, default=datetime.utcnow)
-
-    company = relationship("Company", back_populates="business_types")
-    business_type = relationship("BusinessType", back_populates="company_business_types")
-
-class OfferWorkingType(Base):
-    __tablename__ = 'offer_working_type'
+class OfferRequirements(Base):
+    __tablename__ = 'offer_requirements'
 
     id = Column(Integer, primary_key=True)
-    id_working_type = Column(Integer, ForeignKey('working_type.id'))
     id_offer = Column(Integer, ForeignKey('offer.id'))
+    id_requirements = Column(Integer, ForeignKey('requirements.id'))
+    obligatory = Column(Boolean)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
 
-    working_type = relationship("WorkingType", back_populates="offer_working_types")
-    offer = relationship("Offer", back_populates="offer_working_types")
+    offer = relationship("Offer", back_populates="requirements")
+    requirements = relationship("Requirements", back_populates="offers")
