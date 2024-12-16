e50bcca55f913d03d4a1e6deb01c275fffaafdb6
VladimirKrutko <krutkovova24@gmail.com>
2024-12-16 20:56:43 +0100

    Refactor BaseParser to introduce RESULT_TEMPLATE for structured data representation and enforce abstract methods for variable initialization and parsing logic.

--- scripting/parser/base_parser.py
+++ scripting/parser/base_parser.py
@@ -1,11 +1,31 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class BaseParser(ABC):
+    RESULT_TEMPLATE = {
+        'url': None,
+        'site': 'pracuj.pl',
+        'company_name': None,
+        'company_url': None,
+        'company_description': None,
+        'offer_title': None,
+        'position_level': None,
+        'technology_list': None,
+        'offer_description': None,
+        'requirements': None,
+        'responsibilities': None,
+        'language': None,
+        'city': None,
+        'salary': None,
+        'work_type': None,
+    }
+    
     def __init__(self, parsed_site):
         self.parsed_site = parsed_site
-        
+    
+    @abstractmethod
     def initalize_variables():
         pass
-
-    async def parse(self, response_result):
+    
+    @abstractmethod
+    def parse(self, response_result):
         return dict
