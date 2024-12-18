999fb9bcdacc44489c0251f650ecd807389626e5
VladimirKrutko <krutkovova24@gmail.com>
2024-12-18 19:46:58 +0100

    update import in base_model

--- scripting/loader/base_model.py
+++ scripting/loader/base_model.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from scripting.loader.db_setup import Base, Session
-from db_setup import Session as SessionLocal
 from sqlalchemy import (
     Column, Integer, String, Boolean, DateTime, JSON, ForeignKey, Text, UniqueConstraint
 )
@@ -16,7 +15,7 @@ class BaseModel:
 
     @classmethod
     def create(cls, **kwargs):
-        session = SessionLocal()
+        session = Session()
         if cls.exists(**kwargs):
             print(f"{cls.__name__} already exists!")
             return None
@@ -28,7 +27,7 @@ class BaseModel:
 
     @classmethod
     def update(cls, record_id, **kwargs):
-        session = SessionLocal()
+        session = Session()
         instance = session.query(cls).filter_by(id=record_id).first()
         if not instance:
             print(f"{cls.__name__} with id {record_id} not found.")
@@ -41,5 +40,5 @@ class BaseModel:
 
     @classmethod
     def exists(cls, **kwargs):
-        session = SessionLocal()
+        session = Session()
         return session.query(cls).filter_by(**kwargs).first() is not None
