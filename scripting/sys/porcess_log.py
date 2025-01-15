e98eb54079f740aaaf19d8e5d4732f6aaf9d7a08
VladimirKrutko <krutkovova24@gmail.com>
2025-01-15 20:21:21 +0100

    Add Logger class for process-based logging functionality

---
+++ scripting/sys/porcess_log.py
@@ -0,0 +1,17 @@
+from MORE_JOBS.scripting.loader.models import crawl_log, loader_log, parser_log
+"""
+This is a simple logger class that logs the data based on the process name.
+"""
+class Logger:
+    def __init__(self, process_name):
+        self.process_name = process_name
+
+    def log(self, **kwargs):
+        if self.process_name == 'crawl':
+            crawl_log.CrawlLog.create(**kwargs)
+        elif self.process_name == 'loader':
+            loader_log.LoaderLog.create(**kwargs)
+        elif self.process_name == 'parser':
+            parser_log.ParserLog.create(**kwargs)
+        else:
+            raise ValueError('Invalid process name')
